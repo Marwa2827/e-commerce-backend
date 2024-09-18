7a76fba46259070f1517621740093ec2ab9bb95b
Marwa2827 <marwa.ahmed2813@gmail.com>
2024-09-18 20:43:12 +0300

    add Model

---
+++ e-commerce-backend/Models/product.cs
@@ -0,0 +1,9 @@
+﻿namespace e_commerce_backend.Models
+{
+    public class product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
