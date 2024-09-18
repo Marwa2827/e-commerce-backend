a165cd7e2f502ebf245527fb9a01bc0833b85d02
Marwa2827 <marwa.ahmed2813@gmail.com>
2024-09-18 21:59:51 +0300

    Database connection

--- e-commerce-backend/Models/productContext.cs
+++ e-commerce-backend/Models/productContext.cs
@@ -4,9 +4,14 @@ namespace e_commerce_backend.Models
 {
     public class productContext : DbContext
     {
-        public productContext(DbContextOptions<productContext> options)
-        : base(options)
+        public IConfiguration _config { get; set; }
+        public productContext(IConfiguration config)
         {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
         }
 
         public DbSet<product> products { get; set; } = null!;
