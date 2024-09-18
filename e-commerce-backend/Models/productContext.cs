31f4695725b2da95a5be5d35a1a9ac3d58ee3e10
Marwa2827 <marwa.ahmed2813@gmail.com>
2024-09-18 20:50:25 +0300

    add productContext

---
+++ e-commerce-backend/Models/productContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce_backend.Models
+{
+    public class productContext : DbContext
+    {
+        public productContext(DbContextOptions<productContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<product> products { get; set; } = null!;
+    }
+}
--- e-commerce-backend/Program.cs
+++ e-commerce-backend/Program.cs
@@ -1,8 +1,13 @@
+using e_commerce_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<productContext>(opt =>
+    opt.UseInMemoryDatabase("e-commerce"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
